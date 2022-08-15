e7846822a7a97a389d1c90c83f435138a534b310
Scott Barclay <sbrcly@gmail.com>
2022-08-14 18:11:54 -0700

    moved projects to js

--- app.js
+++ app.js
@@ -34,9 +34,56 @@ const skills = ['JavaScript', 'Node.js', 'Python', 'HTML & CSS', 'MySQL', 'BigQu
 
 // SECTION 2 SELECTORS
 const section2 = document.querySelector('#Projects');
-const projects = document.querySelectorAll('.project');
-const otherProjects = document.querySelector('.other-projects');
-const imgOverlay = document.querySelectorAll('.image-overlay');
+
+const mainProjects = [
+    {
+        name: 'Arbitrage Calculator',
+        gh_link: 'https://github.com/sbrcly/Arbitrage-Public',
+        img: '/with_arbs3.png',
+        alt: 'Photo of Arbitrage Project',
+        pub_priv: '(Private)',
+        description: 'A sports trading tool that compares Caesars odds against other books to find any potential arbitrage betting opportunities. Updates every minute.',
+        skills_used: ['JavaScript', 'Node.js', 'Express', 'Socket.io', 'API', 'MySQL', 'GCP'],
+        disabled: true
+    },
+    {
+        name: 'Sports Trading Schedule',
+        gh_link: 'https://github.com/sbrcly/Trading-Schedule-Public',
+        img: '/trade_sched.png',
+        alt: 'Photo of Trading Schedule',
+        pub_priv: '(Private)',
+        description: 'A scheduling application for sports traders. Pulls in game data and assigns traders to those games.',
+        skills_used: ['JavaScript', 'Node.js', 'Express', 'Socket.io', 'API', 'MySQL', 'GCP'],
+        disabled: true
+    },
+    {
+        name: 'In-Game Odds Tracker',
+        gh_link: 'https://github.com/sbrcly/Odds-Tracker-Public',
+        img: '/odds_visual.png',
+        alt: 'Photo of In-Game Odds Tracker',
+        pub_priv: '(Private)',
+        description: 'A sports trading tool that tracks, stores and graphs the in-game odds for Caesars and their competitors on a second by second basis.',
+        skills_used: ['Node.js', 'MySQL', 'GCP', 'Express', 'API', 'Python', 'Excel'],
+        disabled: true
+    },
+];
+
+const portfolioArchives = [
+    {
+        name: 'Personal Portfolio V1',
+        description: `This is the first portfolio I ever made. I no longer maintain this project so it's likely some features will no longer function as intended.`,
+        skills_used: ['JavaScript', 'HTML', 'CSS'],
+        liveLink: 'http://smbarclay.com',
+        gh_link: `https://github.com/sbrcly`,
+    },
+    {
+        name: 'Personal Portfolio V2',
+        description: `This is the second iteration of my portfolio. I no longer maintain this project so it's likely some features will no longer function as intended.`,
+        skills_used: ['JavaScript', 'HTML', 'CSS', 'API'],
+        liveLink: 'http://sbrcly.com',
+        gh_link: `https://github.com/sbrcly`,
+    }
+]
 
 // FOOTER SELECTORS
 const footer = document.querySelector('footer');
@@ -102,9 +149,12 @@ class CreatePage {
         this.originalPosition = header.getBoundingClientRect().top;
 
         this.createNav();
-        this.scrollOnLoad();
         this.createListeners();
         this.appendSkills();
+        this.appendMainProjects();
+        this.appendPortfolioArchives();
+        this.scrollOnLoad();
+        
     }
     createNav = () => {
         let linkNum = 1;
@@ -251,6 +301,196 @@ class CreatePage {
             };
         };
     }
+    appendMainProjects = () => {
+        for (let i = 0; i < mainProjects.length; i++) {
+            if (i % 2 === 0) {
+                const projectDiv = document.createElement('div');
+                projectDiv.classList.add('project');
+                projectDiv.classList.add(`project-${i+1}`);
+    
+                const projectLeft = document.createElement('div');
+                projectLeft.classList.add(`project-${i+1}-left`);
+                projectDiv.append(projectLeft);
+    
+                const imgBacklay = document.createElement('div');
+                imgBacklay.classList.add('image-backlay');
+    
+                const imgLink = document.createElement('a');
+                imgLink.setAttribute('href', mainProjects[i].gh_link);
+                imgLink.setAttribute('target', '_blank');
+                imgLink.innerHTML = `<img src="${mainProjects[i].img}" alt="${mainProjects[i].alt}">`;
+    
+                const imgOverlay = document.createElement('div');
+                imgOverlay.classList.add('image-overlay');
+    
+                projectLeft.append(imgBacklay, imgLink, imgOverlay);
+    
+                const projectRight = document.createElement('div');
+                projectRight.classList.add(`project-${i+1}-right`);
+                projectDiv.append(projectRight);
+    
+                const subHeading = document.createElement('p');
+                subHeading.classList.add('sub-heading');
+                subHeading.innerText = 'Latest Project';
+    
+                const projectHeading = document.createElement('h3');
+                projectHeading.innerHTML = `${mainProjects[i].name} <span>${mainProjects[i].pub_priv}</span>`;
+    
+                const projectDescription = document.createElement('div');
+                projectDescription.classList.add('project-description');
+                projectDescription.innerHTML = `<p>${mainProjects[i].description}</p>`;
+    
+                const projectSkills = document.createElement('div');
+                projectSkills.classList.add('tools-used');
+                projectSkills.innerHTML = `<p><span>${mainProjects[i].skills_used.join('</span><span>')}</span></p>`;
+    
+                const projectLinks = document.createElement('div');
+                projectLinks.classList.add('project-links');
+    
+                projectRight.append(subHeading, projectHeading, projectDescription, projectSkills, projectLinks);
+    
+                const gitHubLink = document.createElement('a');
+                gitHubLink.setAttribute('href', mainProjects[i].gh_link);
+                gitHubLink.setAttribute('target', '_blank');
+                gitHubLink.classList.add('project-code');
+                gitHubLink.innerHTML = `<i class="fa-brands fa-github-alt"></i>`;
+    
+                const liveLink = document.createElement('i');
+                liveLink.classList.add('fa-solid');
+                liveLink.classList.add('fa-arrow-up-right-from-square');
+                liveLink.classList.add('project-link');
+                if (mainProjects[i].disabled) liveLink.classList.add('disabled-link');
+    
+                projectLinks.append(gitHubLink, liveLink);
+                section2.append(projectDiv);
+            }   else {
+                const projectDiv = document.createElement('div');
+                projectDiv.classList.add('project');
+                projectDiv.classList.add(`project-${i+1}`);
+    
+                const projectRight = document.createElement('div');
+                projectRight.classList.add(`project-${i+1}-right`);
+                
+    
+                const imgBacklay = document.createElement('div');
+                imgBacklay.classList.add('image-backlay');
+    
+                const imgLink = document.createElement('a');
+                imgLink.setAttribute('href', mainProjects[i].gh_link);
+                imgLink.setAttribute('target', '_blank');
+                imgLink.innerHTML = `<img src="${mainProjects[i].img}" alt="${mainProjects[i].alt}">`;
+    
+                const imgOverlay = document.createElement('div');
+                imgOverlay.classList.add('image-overlay');
+    
+                projectRight.append(imgBacklay, imgLink, imgOverlay);
+    
+                const projectLeft = document.createElement('div');
+                projectLeft.classList.add(`project-${i+1}-left`);
+    
+                const subHeading = document.createElement('p');
+                subHeading.classList.add('sub-heading');
+                subHeading.innerText = 'Latest Project';
+    
+                const projectHeading = document.createElement('h3');
+                projectHeading.innerHTML = `${mainProjects[i].name} <span>${mainProjects[i].pub_priv}</span>`;
+    
+                const projectDescription = document.createElement('div');
+                projectDescription.classList.add('project-description');
+                projectDescription.innerHTML = `<p>${mainProjects[i].description}</p>`;
+    
+                const projectSkills = document.createElement('div');
+                projectSkills.classList.add('tools-used');
+                projectSkills.innerHTML = `<p><span>${mainProjects[i].skills_used.join('</span><span>')}</span></p>`;
+    
+                const projectLinks = document.createElement('div');
+                projectLinks.classList.add('project-links');
+    
+                projectLeft.append(subHeading, projectHeading, projectDescription, projectSkills, projectLinks);
+    
+                const gitHubLink = document.createElement('a');
+                gitHubLink.setAttribute('href', mainProjects[i].gh_link);
+                gitHubLink.setAttribute('target', '_blank');
+                gitHubLink.classList.add('project-code');
+                gitHubLink.innerHTML = `<i class="fa-brands fa-github-alt"></i>`;
+    
+                const liveLink = document.createElement('i');
+                liveLink.classList.add('fa-solid');
+                liveLink.classList.add('fa-arrow-up-right-from-square');
+                liveLink.classList.add('project-link');
+                if (mainProjects[i].disabled) liveLink.classList.add('disabled-link');
+    
+                projectLinks.append(gitHubLink, liveLink);
+                projectDiv.append(projectLeft, projectRight);
+                section2.append(projectDiv);
+            }
+        }
+    }
+    appendPortfolioArchives = () => {
+        const otherProjects = document.createElement('div');
+        otherProjects.classList.add('project');
+        otherProjects.classList.add('other-projects');
+
+        const otherProjectsHeader = document.createElement('h3');
+        otherProjectsHeader.innerText = 'Portfolio Archives';
+
+        const otherProjectsP1 = document.createElement('p');
+        otherProjectsP1.classList.add('other-project-blurb');
+        otherProjectsP1.innerText = `All old projects are contained in these archived portfolios.`;
+        
+        const otherProjectsP2 = document.createElement('p');
+        otherProjectsP2.classList.add('other-project-blurb');
+        otherProjectsP2.innerText = `New projects will be added to the list above.`;
+
+        const otherProjectTiles = document.createElement('div');
+        otherProjectTiles.classList.add('other-project-tiles');
+
+        otherProjects.append(otherProjectsHeader, otherProjectsP1, otherProjectsP2, otherProjectTiles);
+
+        for (let i = 0; i < portfolioArchives.length; i++) {
+            const portfolioDiv = document.createElement('div');
+            portfolioDiv.classList.add('other-project');
+            portfolioDiv.classList.add(`other-project-${i+1}`);
+
+            const portfolioHeader = document.createElement('div');
+            portfolioHeader.classList.add('other-project-header');
+            portfolioDiv.append(portfolioHeader);
+
+            const gitHubLink = document.createElement('a');
+            gitHubLink.setAttribute('href', portfolioArchives[i].gh_link);
+            gitHubLink.setAttribute('target', '_blank');
+            gitHubLink.classList.add('project-code');
+            gitHubLink.innerHTML = `<i class="fa-brands fa-github-alt"></i>`;
+
+            const liveLink = document.createElement('a');
+            liveLink.setAttribute('href', portfolioArchives[i].liveLink);
+            liveLink.setAttribute('target', '_blank');
+            liveLink.innerHTML = `<i class="fa-solid fa-arrow-up-right-from-square project-link"></i>`;
+
+            portfolioHeader.append(gitHubLink, liveLink);
+
+            const portfolioBody = document.createElement('div');
+            portfolioBody.classList.add('other-project-body');
+            portfolioDiv.append(portfolioBody);
+
+            const portfolioTitle = document.createElement('h4');
+            portfolioTitle.innerText = portfolioArchives[i].name;
+
+            const portfolioDescription = document.createElement('p');
+            portfolioDescription.innerText = portfolioArchives[i].description;
+
+            portfolioBody.append(portfolioTitle, portfolioDescription);
+
+            const portfolioSkills = document.createElement('div');
+            portfolioSkills.classList.add('other-project-tech');
+            portfolioSkills.innerHTML = `<p><span>${portfolioArchives[i].skills_used.join('</span><span>')}</span></p>`;
+            portfolioDiv.append(portfolioSkills);
+
+            otherProjectTiles.append(portfolioDiv);            
+        }
+
+        section2.append(otherProjects);
+    }
     scrollActions = (e) => {
         if (header.getBoundingClientRect().top > this.lastScrollTop &&
             header.getBoundingClientRect().top <= logo.getBoundingClientRect().top) {
@@ -282,7 +522,7 @@ class CreatePage {
                 };
             }
         };
-
+        const projects = document.querySelectorAll('.project');
         for (let project of projects) {
             if (window.innerWidth > 2000) {
                 if (project.getBoundingClientRect().top <= 1200) {
